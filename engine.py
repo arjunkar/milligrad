fba09dc72fc097c24ac886227bf247613322b9a1
Arjun Kar <arjunkar0@gmail.com>
2023-02-15 14:25:33 -0800

    First commit

---
+++ engine.py
@@ -0,0 +1,9 @@
+"""
+An implementation of backpropagation in the style of
+PyTorch for a single scalar variable.
+
+Inspired by Karpathy's micrograd, much of the code is
+taken directly from that project.
+https://github.com/karpathy/micrograd
+"""
+
---
+++ nn.py
@@ -0,0 +1,12 @@
+"""
+Implementation of various layers needed to define a
+simple feedforward neural network.
+
+Built from the scalar Value class which allows for
+training by backpropagation in the PyTorch style.
+
+Largely taken from Karpathy's micrograd project, an
+educational resource for understanding the high level
+functioning of a modern autograd engine.
+https://github.com/karpathy/micrograd
+"""
