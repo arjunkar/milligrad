e8e572fd616e09fa4d0feb4eb23f3c6e9712f5c0
Arjun Kar <arjunkar0@gmail.com>
2023-02-19 00:16:07 -0800

    Revert foldering again, create optim

---
+++ optim.py
@@ -0,0 +1,9 @@
+"""
+A basic optimizer for use with milligrad's autograd engine
+and neural network library.
+
+Designed to follow PyTorch's optim class.
+
+Implements stochastic gradient descent, momentum, and other
+common optimization schemes.
+"""
