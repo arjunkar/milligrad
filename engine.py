ad9d151578f545e8a4e67b190b92e151014506bf
Arjun Kar <arjunkar0@gmail.com>
2023-02-18 21:16:29 -0800

    Engine improvements

--- engine.py
+++ engine.py
@@ -1,5 +1,5 @@
 """
-An implementation of backpropagation in the style of
+An implementation of automatic differentiation in the style of
 PyTorch for a single tensor variable.
 
 Inspired by Karpathy's micrograd, much of the code is
@@ -10,6 +10,8 @@ We will support operations involved in a simple feedforward network
 that uses ReLU activation functions.
 This includes weight matrix multiplication, bias vector addition,
 and element-wise ReLU activation.
+Furthermore, we will implement a cross entropy loss which requires
+further support of indexing, arithmetic, axis sums, and exp and log functions.
 """
 
 import numpy as np
@@ -17,36 +19,102 @@ import numpy as np
 class Tensor:
 
     def __init__(self, data, _children=()) -> None:
-        self.data = data if isinstance(data, np.ndarray) else np.array(data)
-        self.grad = np.zeros_like(data)
+        self.data = data if isinstance(data, np.ndarray) else np.array(data, dtype='float32')
+        self.grad = np.zeros_like(data, dtype='float32')
         self.shape = self.data.shape
         # To construct the autograd graph, several internal
         # variables are needed.
         self._backward = lambda: None
         self._prev = set(_children)
 
+    def __getitem__(self, key):
+        out = Tensor(self.data[key], (self,))
+
+        def _backward():
+            grads = np.zeros_like(self.grad)
+            grads[key] = out.grad
+            self.grad += grads
+        out._backward = _backward
+
+        return out
+
     def __add__(self, other):
-        # Sizes of self and other must allow addition broadcasting
+        # Sizes of self and other must allow broadcasting
         other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data + other.data, (self, other))
 
         def _backward():
             def shape_sum(tensor: Tensor):
-                if tensor.shape == out.shape:
-                    tensor.grad += out.grad
-                else: # tensor was broadcasted to facilitate addition into out
-                    diff = len(out.shape) - len(tensor.shape)
-                    pos = [axis for axis,dim in 
-                        enumerate([1 for _ in range(diff)] + list(tensor.shape))
-                        if dim == 1]
-                    tensor.grad += np.sum(out.grad, axis=tuple(pos)).reshape(tensor.shape)
-                    # tensor position receives gradients from all out positions
-                    # over which it was broadcasted
+                # tensor may be broadcasted to facilitate addition into out
+                diff = len(out.shape) - len(tensor.shape)
+                pos = [axis for axis,dim in 
+                    enumerate([1 for _ in range(diff)] + list(tensor.shape))
+                    if dim == 1]
+                tensor.grad += np.sum(out.grad, axis=tuple(pos)).reshape(tensor.shape)
+                # tensor position receives gradients from all out positions
+                # over which it was broadcasted
             shape_sum(self)
             shape_sum(other)
         out._backward = _backward
 
         return out
+
+    def __neg__(self): 
+        # define -self
+        return self * -1
+
+    def __sub__(self, other): 
+        # subtract based on negation addition
+        return self + (-other)
+
+    def __mul__(self, other):
+        # Sizes of self and other must allow broadcasting
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data * other.data, (self, other))
+
+        def _backward():
+            def shape_mul(tensor1: Tensor, tensor2: Tensor):
+                # tensor may be broadcasted to facilitate multiplication into out
+                diff = len(out.shape) - len(tensor1.shape)
+                pos = [axis for axis,dim in 
+                    enumerate([1 for _ in range(diff)] + list(tensor1.shape))
+                    if dim == 1]
+                tensor1.grad += np.sum(out.grad * tensor2.data, axis=tuple(pos)).reshape(tensor1.shape)
+                # tensor position receives gradients from all out positions
+                # over which it was broadcasted
+            shape_mul(self, other)
+            shape_mul(other, self)
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        # broadcasting power follows same rules as other arithmetic
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data ** other.data, (self, other))
+
+        def _backward():
+            # self ** other derivative w.r.t self is other * self**(other-1)
+            selfdiff = len(out.shape) - len(self.shape)
+            selfpos = [axis for axis,dim in 
+                enumerate([1 for _ in range(selfdiff)] + list(self.shape))
+                if dim == 1]
+            self.grad += np.sum(out.grad * other.data * self.data**(other.data-1), 
+                            axis=tuple(selfpos)).reshape(self.shape)
+            # self ** other derivative w.r.t other is log(self) * self**other
+            otherdiff = len(out.shape) - len(other.shape)
+            otherpos = [axis for axis,dim in 
+                enumerate([1 for _ in range(otherdiff)] + list(other.shape))
+                if dim == 1]
+            other.grad += np.sum(out.grad * np.log(self.data) * out.data, 
+                            axis=tuple(otherpos)).reshape(other.shape)
+        out._backward = _backward
+
+        return out
+
+    def __truediv__(self, other):
+        # broadcasting self / other using pow = -1
+        return self * other**-1
 
     def matmul(self, other):
         # Sizes of self and other must allow matrix multiplication and broadcasting.
@@ -77,11 +145,42 @@ class Tensor:
 
         return out
 
+    def sum(self, axis=None):
+        broadcast_summable = np.sum(self.data, axis=axis, keepdims=True)
+        out = Tensor(np.squeeze(broadcast_summable, axis=axis), (self,))
+
+        def _backward():
+            # broadcast addition to pass gradients to input
+            self.grad += out.grad.reshape(broadcast_summable.shape)
+        out._backward = _backward
+
+        return out
+
     def relu(self):
         out = Tensor(np.maximum(self.data, 0.), (self,))
 
         def _backward():
             self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def exp(self):
+        out = Tensor(np.exp(self.data), (self,))
+
+        def _backward():
+            # derivative of exp(a) w.r.t a is exp(a)
+            self.grad += out.grad * out.data
+        out._backward = _backward
+
+        return out
+
+    def log(self):
+        out = Tensor(np.log(self.data), (self,))
+
+        def _backward():
+            # derivative of log(a) w.r.t a is 1/a
+            self.grad += out.grad * self.data**-1
         out._backward = _backward
 
         return out
--- test_engine.py
+++ test_engine.py
@@ -4,6 +4,41 @@ import numpy as np
 
 np.random.seed(1234)
 
+def test_getitem():
+    a1 = np.single(np.random.random_sample(size=(2,3,4)))
+    grad = np.single(np.random.random_sample(size=(3,4)))
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    (ta1[0]).backward(gradient=torch.tensor(grad))
+    ea1 = engine.Tensor(a1)
+    (ea1[0]).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+
+    grad = np.single(np.random.random_sample(size=4))
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    (ta1[0][1]).backward(gradient=torch.tensor(grad))
+    ea1 = engine.Tensor(a1)
+    (ea1[0][1]).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+
+    grad = np.single(np.random.random_sample())
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    (ta1[0][1][2]).backward(gradient=torch.tensor(grad))
+    ea1 = engine.Tensor(a1)
+    (ea1[0][1][2]).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+
 def test_add():
     a1 = np.single(np.random.random_sample(size=(2,3,4)))
     a2 = np.single(np.random.random_sample(size=(2,3,4)))
@@ -43,6 +78,254 @@ def test_add():
     (ea1+eb2).backward(gradient=grad)
     (ea1+eb3).backward(gradient=grad)
     (ea1+eb4).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+    assert(
+        torch.allclose(ta2.grad, torch.tensor(ea2.grad))
+    )
+    assert(
+        torch.allclose(tb0.grad, torch.tensor(eb0.grad))
+    )
+    assert(
+        torch.allclose(tb1.grad, torch.tensor(eb1.grad))
+    )
+    assert(
+        torch.allclose(tb2.grad, torch.tensor(eb2.grad))
+    )
+    assert(
+        torch.allclose(tb3.grad, torch.tensor(eb3.grad))
+    )
+    assert(
+        torch.allclose(tb4.grad, torch.tensor(eb4.grad))
+    )
+
+def test_sub():
+    a1 = np.single(np.random.random_sample(size=(2,3,4)))
+    a2 = np.single(np.random.random_sample(size=(2,3,4)))
+    b0 = np.single(np.random.random_sample())
+    b1 = np.single(np.random.random_sample(size=4))
+    b2 = np.single(np.random.random_sample(size=(3,1)))
+    b3 = np.single(np.random.random_sample(size=(2,1,1)))
+    b4 = np.single(np.random.random_sample(size=(2,1,4)))
+    grad = np.single(np.random.random_sample(size=(2,3,4)))
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    ta2 = torch.tensor(a2, requires_grad=True)
+    tb0 = torch.tensor(b0, requires_grad=True)
+    tb1 = torch.tensor(b1, requires_grad=True)
+    tb2 = torch.tensor(b2, requires_grad=True)
+    tb3 = torch.tensor(b3, requires_grad=True)
+    tb4 = torch.tensor(b4, requires_grad=True)
+
+    (ta1-ta2).backward(gradient=torch.tensor(grad))
+    (ta1-tb0).backward(gradient=torch.tensor(grad))
+    (ta1-tb1).backward(gradient=torch.tensor(grad))
+    (ta1-tb2).backward(gradient=torch.tensor(grad))
+    (ta1-tb3).backward(gradient=torch.tensor(grad))
+    (ta1-tb4).backward(gradient=torch.tensor(grad))
+
+    ea1 = engine.Tensor(a1)
+    ea2 = engine.Tensor(a2)
+    eb0 = engine.Tensor(b0)
+    eb1 = engine.Tensor(b1)
+    eb2 = engine.Tensor(b2)
+    eb3 = engine.Tensor(b3)
+    eb4 = engine.Tensor(b4)
+
+    (ea1-ea2).backward(gradient=grad)
+    (ea1-eb0).backward(gradient=grad)
+    (ea1-eb1).backward(gradient=grad)
+    (ea1-eb2).backward(gradient=grad)
+    (ea1-eb3).backward(gradient=grad)
+    (ea1-eb4).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+    assert(
+        torch.allclose(ta2.grad, torch.tensor(ea2.grad))
+    )
+    assert(
+        torch.allclose(tb0.grad, torch.tensor(eb0.grad))
+    )
+    assert(
+        torch.allclose(tb1.grad, torch.tensor(eb1.grad))
+    )
+    assert(
+        torch.allclose(tb2.grad, torch.tensor(eb2.grad))
+    )
+    assert(
+        torch.allclose(tb3.grad, torch.tensor(eb3.grad))
+    )
+    assert(
+        torch.allclose(tb4.grad, torch.tensor(eb4.grad))
+    )
+
+def test_mul():
+    a1 = np.single(np.random.random_sample(size=(2,3,4)))
+    a2 = np.single(np.random.random_sample(size=(2,3,4)))
+    b0 = np.single(np.random.random_sample())
+    b1 = np.single(np.random.random_sample(size=4))
+    b2 = np.single(np.random.random_sample(size=(3,1)))
+    b3 = np.single(np.random.random_sample(size=(2,1,1)))
+    b4 = np.single(np.random.random_sample(size=(2,1,4)))
+    grad = np.single(np.random.random_sample(size=(2,3,4)))
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    ta2 = torch.tensor(a2, requires_grad=True)
+    tb0 = torch.tensor(b0, requires_grad=True)
+    tb1 = torch.tensor(b1, requires_grad=True)
+    tb2 = torch.tensor(b2, requires_grad=True)
+    tb3 = torch.tensor(b3, requires_grad=True)
+    tb4 = torch.tensor(b4, requires_grad=True)
+
+    (ta1*ta2).backward(gradient=torch.tensor(grad))
+    (ta1*tb0).backward(gradient=torch.tensor(grad))
+    (ta1*tb1).backward(gradient=torch.tensor(grad))
+    (ta1*tb2).backward(gradient=torch.tensor(grad))
+    (ta1*tb3).backward(gradient=torch.tensor(grad))
+    (ta1*tb4).backward(gradient=torch.tensor(grad))
+
+    ea1 = engine.Tensor(a1)
+    ea2 = engine.Tensor(a2)
+    eb0 = engine.Tensor(b0)
+    eb1 = engine.Tensor(b1)
+    eb2 = engine.Tensor(b2)
+    eb3 = engine.Tensor(b3)
+    eb4 = engine.Tensor(b4)
+
+    (ea1*ea2).backward(gradient=grad)
+    (ea1*eb0).backward(gradient=grad)
+    (ea1*eb1).backward(gradient=grad)
+    (ea1*eb2).backward(gradient=grad)
+    (ea1*eb3).backward(gradient=grad)
+    (ea1*eb4).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+    assert(
+        torch.allclose(ta2.grad, torch.tensor(ea2.grad))
+    )
+    assert(
+        torch.allclose(tb0.grad, torch.tensor(eb0.grad))
+    )
+    assert(
+        torch.allclose(tb1.grad, torch.tensor(eb1.grad))
+    )
+    assert(
+        torch.allclose(tb2.grad, torch.tensor(eb2.grad))
+    )
+    assert(
+        torch.allclose(tb3.grad, torch.tensor(eb3.grad))
+    )
+    assert(
+        torch.allclose(tb4.grad, torch.tensor(eb4.grad))
+    )
+
+def test_div():
+    a1 = np.single(np.random.random_sample(size=(2,3,4)))
+    a2 = np.single(np.random.random_sample(size=(2,3,4)))
+    b0 = np.single(np.random.random_sample())
+    b1 = np.single(np.random.random_sample(size=4))
+    b2 = np.single(np.random.random_sample(size=(3,1)))
+    b3 = np.single(np.random.random_sample(size=(2,1,1)))
+    b4 = np.single(np.random.random_sample(size=(2,1,4)))
+    grad = np.single(np.random.random_sample(size=(2,3,4)))
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    ta2 = torch.tensor(a2, requires_grad=True)
+    tb0 = torch.tensor(b0, requires_grad=True)
+    tb1 = torch.tensor(b1, requires_grad=True)
+    tb2 = torch.tensor(b2, requires_grad=True)
+    tb3 = torch.tensor(b3, requires_grad=True)
+    tb4 = torch.tensor(b4, requires_grad=True)
+
+    (ta1/ta2).backward(gradient=torch.tensor(grad))
+    (ta1/tb0).backward(gradient=torch.tensor(grad))
+    (ta1/tb1).backward(gradient=torch.tensor(grad))
+    (ta1/tb2).backward(gradient=torch.tensor(grad))
+    (ta1/tb3).backward(gradient=torch.tensor(grad))
+    (ta1/tb4).backward(gradient=torch.tensor(grad))
+
+    ea1 = engine.Tensor(a1)
+    ea2 = engine.Tensor(a2)
+    eb0 = engine.Tensor(b0)
+    eb1 = engine.Tensor(b1)
+    eb2 = engine.Tensor(b2)
+    eb3 = engine.Tensor(b3)
+    eb4 = engine.Tensor(b4)
+
+    (ea1/ea2).backward(gradient=grad)
+    (ea1/eb0).backward(gradient=grad)
+    (ea1/eb1).backward(gradient=grad)
+    (ea1/eb2).backward(gradient=grad)
+    (ea1/eb3).backward(gradient=grad)
+    (ea1/eb4).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+    assert(
+        torch.allclose(ta2.grad, torch.tensor(ea2.grad))
+    )
+    assert(
+        torch.allclose(tb0.grad, torch.tensor(eb0.grad))
+    )
+    assert(
+        torch.allclose(tb1.grad, torch.tensor(eb1.grad))
+    )
+    assert(
+        torch.allclose(tb2.grad, torch.tensor(eb2.grad))
+    )
+    assert(
+        torch.allclose(tb3.grad, torch.tensor(eb3.grad))
+    )
+    assert(
+        torch.allclose(tb4.grad, torch.tensor(eb4.grad))
+    )
+
+def test_pow():
+    a1 = np.single(np.random.random_sample(size=(2,3,4)))
+    a2 = np.single(np.random.random_sample(size=(2,3,4)))
+    b0 = np.single(np.random.random_sample())
+    b1 = np.single(np.random.random_sample(size=4))
+    b2 = np.single(np.random.random_sample(size=(3,1)))
+    b3 = np.single(np.random.random_sample(size=(2,1,1)))
+    b4 = np.single(np.random.random_sample(size=(2,1,4)))
+    grad = np.single(np.random.random_sample(size=(2,3,4)))
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    ta2 = torch.tensor(a2, requires_grad=True)
+    tb0 = torch.tensor(b0, requires_grad=True)
+    tb1 = torch.tensor(b1, requires_grad=True)
+    tb2 = torch.tensor(b2, requires_grad=True)
+    tb3 = torch.tensor(b3, requires_grad=True)
+    tb4 = torch.tensor(b4, requires_grad=True)
+
+    (ta1**ta2).backward(gradient=torch.tensor(grad))
+    (ta1**tb0).backward(gradient=torch.tensor(grad))
+    (ta1**tb1).backward(gradient=torch.tensor(grad))
+    (ta1**tb2).backward(gradient=torch.tensor(grad))
+    (ta1**tb3).backward(gradient=torch.tensor(grad))
+    (ta1**tb4).backward(gradient=torch.tensor(grad))
+
+    ea1 = engine.Tensor(a1)
+    ea2 = engine.Tensor(a2)
+    eb0 = engine.Tensor(b0)
+    eb1 = engine.Tensor(b1)
+    eb2 = engine.Tensor(b2)
+    eb3 = engine.Tensor(b3)
+    eb4 = engine.Tensor(b4)
+
+    (ea1**ea2).backward(gradient=grad)
+    (ea1**eb0).backward(gradient=grad)
+    (ea1**eb1).backward(gradient=grad)
+    (ea1**eb2).backward(gradient=grad)
+    (ea1**eb3).backward(gradient=grad)
+    (ea1**eb4).backward(gradient=grad)
 
     assert(
         torch.allclose(ta1.grad, torch.tensor(ea1.grad))
@@ -142,6 +425,35 @@ def test_matmul():
         torch.allclose(tmat2.grad, torch.tensor(emat2.grad))
     )
 
+def test_sum():
+    a1 = np.single(np.random.random_sample(size=(2,3,4)))
+    grad = np.single(np.random.random_sample(size=()))
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    (ta1.sum()).backward(gradient=torch.tensor(grad))
+
+    ea1 = engine.Tensor(a1)
+    (ea1.sum()).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+
+    grad = np.single(np.random.random_sample(size=(2,3)))
+    (ta1.sum(axis=-1)).backward(gradient=torch.tensor(grad))
+    (ea1.sum(axis=-1)).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+
+    grad = np.single(np.random.random_sample(size=(4)))
+    (ta1.sum(axis=(0,1))).backward(gradient=torch.tensor(grad))
+    (ea1.sum(axis=(0,1))).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
 
 def test_relu():
     a1 = np.single(np.random.random_sample(size=(2,3,4)))
@@ -152,6 +464,34 @@ def test_relu():
 
     ea1 = engine.Tensor(a1)
     (ea1.relu()).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+
+def test_exp():
+    a1 = np.single(np.random.random_sample(size=(2,3,4)))
+    grad = np.single(np.random.random_sample(size=(2,3,4)))
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    (ta1.exp()).backward(gradient=torch.tensor(grad))
+
+    ea1 = engine.Tensor(a1)
+    (ea1.exp()).backward(gradient=grad)
+
+    assert(
+        torch.allclose(ta1.grad, torch.tensor(ea1.grad))
+    )
+
+def test_log():
+    a1 = np.single(np.random.random_sample(size=(2,3,4)))
+    grad = np.single(np.random.random_sample(size=(2,3,4)))
+
+    ta1 = torch.tensor(a1, requires_grad=True)
+    (ta1.log()).backward(gradient=torch.tensor(grad))
+
+    ea1 = engine.Tensor(a1)
+    (ea1.log()).backward(gradient=grad)
 
     assert(
         torch.allclose(ta1.grad, torch.tensor(ea1.grad))
